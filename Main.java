f840a1a07eb5f9c3978489365d6cceef6f004302
Konain1 <kaunainkasab@gmail.com>
2023-11-24 20:56:48 +0530

    till n prime

---
+++ Main.java
@@ -0,0 +1,26 @@
+import javax.sound.midi.SysexMessage;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+       int num = 13;
+
+       for(int i = 2 ; i<=num; i++){
+           boolean flag = false;
+
+           for(int j = 2;j<i;j++){
+               if(i % j == 0){
+                 flag = true;
+               }
+           }
+           if(flag ==false){
+               System.out.println(i +" ia a prime number");
+           }
+       }
+
+
+
+
+    }
+}
